0be131067b4fca02142ddeba2edd7ccc8ed1d8ec
C10H12O2 <Eugene.emmanuel2007@gmail.com>
2025-12-17 09:56:27 +0300

    Added a combineUsers function with a spread operator and merge date

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
+
+  for (let i = 0; i < args.length; i++) {
+    combinedObject.users.push(...args[i]);
+  }
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+  return combinedObject;
+}
 
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
+console.log(
+  combineUsers(
+    ["alice", "bob"],
+    ["charlie"],
+    ["david", "eve"]
+  )
+);
